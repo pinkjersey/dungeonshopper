e0f9992f629f3f52e10cade7d80b651b02ca5507
gmkagan <garykagan@gmail.com>
2015-09-10 20:49:21 -0400

    factory update

--- static/factory.js
+++ static/factory.js
@@ -4,7 +4,8 @@ angular.module('dsApp')
                newGame: function (numberOfPlayers, callback, errorcallback) {
                    $http({
                        method: 'GET',
-                       url: 'http://vdknycfl00222:13080/game?action=new&numPlayers=' + numberOfPlayers,
+                       //url: 'http://vdknycfl00222:13080/game?action=new&numPlayers=' + numberOfPlayers,
+					   url: '/game?action=new&numPlayers=' + numberOfPlayers,
                        cache: false
                    }).success(callback)
                      .error(errorcallback);
@@ -12,7 +13,8 @@ angular.module('dsApp')
                joinGame: function (playerId, callback, errorcallback) {
                    $http({
                        method: 'GET',
-                       url: 'http://vdknycfl00222:13080/game?action=join&playerId=' + playerId,
+                       //url: 'http://vdknycfl00222:13080/game?action=join&playerId=' + playerId,
+                       url: '/game?action=join&playerId=' + playerId,
                        cache: false
                    }).success(callback)
                      .error(errorcallback);
